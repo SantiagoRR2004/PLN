181d44958634cab17d0ef3a46af7cd52bdb7a8c4
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-09-16 11:19:45 +0200

    Tasks 3 and 4

--- P0/p0_corpus_stats.py
+++ P0/p0_corpus_stats.py
@@ -21,23 +21,64 @@ with open(
 
 stopwords = [word.strip() for word in stopwords]
 
-# TODO 3: Obtener diccionarios de frecuencias para tokens y tokens que no son stopwords
+# 3: Obtener diccionarios de frecuencias para tokens y tokens que no son stopwords
+fTokens = {}
+fTokensNoStopwords = {}
 
+for document in corpus:
+    tokens = document.split()
+    for token in tokens:
+        # Frecuencia de tokens
+        if token in fTokens:
+            fTokens[token] += 1
+        else:
+            fTokens[token] = 1
+        # Frecuencia de tokens que no son stopwords
+        if token not in stopwords:
+            if token in fTokensNoStopwords:
+                fTokensNoStopwords[token] += 1
+            else:
+                fTokensNoStopwords[token] = 1
 
-# TODO 4: Obtener estadísticas básicas del corpus:
+# 4: Obtener estadísticas básicas del corpus:
 # * Número de documentos
+print(f"Número de documentos: {len(corpus)}")
 # * Número de tokens
+nTokens = sum(fTokens.values())
+print(f"Número de tokens: {nTokens}")
 # * Número de stopwords
+nTokensNoStopwords = sum(fTokensNoStopwords.values())
+nTokensStopwords = nTokens - nTokensNoStopwords
+print(f"Número de stopwords: {nTokensStopwords}")
 # * Número de tokens que no son stopwords
+print(f"Número de tokens que no son stopwords: {nTokensNoStopwords}")
 # * Proporción de stopwords
+print(f"Proporción de stopwords: {nTokensStopwords / nTokens:.2%}")
 # * Tamaño del vocabulario
+print(f"Tamaño del vocabulario: {len(fTokens)}")
 # * Tamaño del vocabulario sin stopwords
+print(f"Tamaño del vocabulario sin stopwords: {len(fTokensNoStopwords)}")
 # * Longitud media de documento
+print(f"Longitud media de documento: {nTokens / len(corpus):.2f}")
 # * Longitud mínima/máxima de documento
+print(f"Longitud mínima de documento: {min(len(doc.split()) for doc in corpus)}")
+print(f"Longitud máxima de documento: {max(len(doc.split()) for doc in corpus)}")
 # * Longitud media de token
+print(f"Longitud media de token: {nTokens / sum(len(token) for token in fTokens):.2f}")
 # * Longitud mínima/máxima de token
+print(f"Longitud mínima de token: {min(len(token) for token in fTokens)}")
+print(f"Longitud máxima de token: {max(len(token) for token in fTokens)}")
 # * Longitud media de tokens sin stopwords
+print(
+    f"Longitud media de tokens sin stopwords: {nTokensNoStopwords / sum(len(token) for token in fTokensNoStopwords):.2f}"
+)
 # * Longitud mínima/máxima de tokens sin stopwords
+print(
+    f"Longitud mínima de tokens sin stopwords: {min(len(token) for token in fTokensNoStopwords)}"
+)
+print(
+    f"Longitud máxima de tokens sin stopwords: {max(len(token) for token in fTokensNoStopwords)}"
+)
 
 # TODO 5: Obtener métricas de riqueza léxica
 # * Type-Token Ratio (TTR)
