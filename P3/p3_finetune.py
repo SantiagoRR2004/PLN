d646c02e346c192278d0552db823134423db3ccc
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-11-18 12:05:22 +0100

    Complete 2 and 4

--- P3/p3_finetune.py
+++ P3/p3_finetune.py
@@ -1,8 +1,16 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    TrainingArguments,
+)
 from datasets import load_dataset
+import torch
 
 # Carga el dataset
 dataset = load_dataset("imdb")
+
+# TODO: Keep only train and test splits (remove unsupervised)
+
 
 # 1: Carga el tokenizador y el modelo a refinar
 model_name = "prajjwal1/bert-tiny"  # or "distilbert-base-uncased"
@@ -10,13 +18,27 @@ model_name = "prajjwal1/bert-tiny"  # or "distilbert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
 
-# TODO 2: Tokeniza el dataset
+# 2: Tokeniza el dataset
+tokenizedDataset = dataset.map(lambda x: tokenizer(x["text"]), batched=True)
 
 # TODO 3: Usa la clase TrainingArguments para definir las opciones de entrenamiento
+trainingArgs = TrainingArguments()
 
-# TODO 4: Implementa una función para obtener la precisión durante la evaluación, a partir del par (logits, labels)
-# logits es una lista de tuplas con las puntuaciones para cada clase
-# labels es una lista con las clases para cada ejemplo de evaluación
+
+def obtainAccuracy(logits: torch.Tensor, labels: torch.Tensor) -> float:
+    """
+    4: Implementa una función para obtener la precisión durante
+    la evaluación, a partir del par (logits, labels)
+
+    Args:
+        - logits (torch.Tensor): lista de tuplas con las puntuaciones para cada clase
+        - labels (torch.Tensor): lista con las clases para cada ejemplo de evaluación
+
+    Returns:
+        - float: precisión del modelo en la evaluación
+    """
+    return (logits.argmax(axis=1) == labels).mean().item()
+
 
 # TODO 5: Usa la clase Trainer para definir un "entrenador" del modelo
 
