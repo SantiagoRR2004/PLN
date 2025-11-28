b9961e139cac45b66bf0e055b864d8244bf5b228
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-11-28 17:54:52 +0100

    Complete finetuning

--- P3/p3_finetune.py
+++ P3/p3_finetune.py
@@ -2,9 +2,16 @@ from transformers import (
     AutoTokenizer,
     AutoModelForSequenceClassification,
     TrainingArguments,
+    Trainer,
 )
 from datasets import load_dataset
-import torch
+import os
+
+# Obtain the current directory
+currentDirectory = os.path.dirname(os.path.abspath(__file__))
+
+# TODO: Check if GPU is available and use it
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 # Carga el dataset
 dataset = load_dataset("imdb")
@@ -18,28 +25,54 @@ model_name = "prajjwal1/bert-tiny"  # or "distilbert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
 
+for param in model.bert.parameters():
+    param.requires_grad = False
+
 # 2: Tokeniza el dataset
-tokenizedDataset = dataset.map(lambda x: tokenizer(x["text"]), batched=True)
+tokenizedDataset = dataset.map(
+    lambda x: tokenizer(
+        x["text"], padding="max_length", truncation=True, max_length=256
+    ),
+    batched=True,
+)
 
-# TODO 3: Usa la clase TrainingArguments para definir las opciones de entrenamiento
-trainingArgs = TrainingArguments()
+# 3: Usa la clase TrainingArguments para definir las opciones de entrenamiento
+trainingArgs = TrainingArguments(
+    output_dir=os.path.join(currentDirectory, "checkpoints"),
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=1,
+)
 
 
-def obtainAccuracy(logits: torch.Tensor, labels: torch.Tensor) -> float:
+def obtainAccuracy(data: tuple) -> dict:
     """
     4: Implementa una función para obtener la precisión durante
     la evaluación, a partir del par (logits, labels)
 
     Args:
-        - logits (torch.Tensor): lista de tuplas con las puntuaciones para cada clase
-        - labels (torch.Tensor): lista con las clases para cada ejemplo de evaluación
+        - data (tuple): A tuple containing:
+            - logits (torch.Tensor): lista de tuplas con las puntuaciones para cada clase
+            - labels (torch.Tensor): lista con las clases para cada ejemplo de evaluación
 
     Returns:
-        - float: precisión del modelo en la evaluación
+        - dict: precisión del modelo en la evaluación
     """
-    return (logits.argmax(axis=1) == labels).mean().item()
+    logits, labels = data[0], data[1]
+    acc = (logits.argmax(axis=1) == labels).mean().item()
+    return {"accuracy": acc}
 
 
-# TODO 5: Usa la clase Trainer para definir un "entrenador" del modelo
+# 5: Usa la clase Trainer para definir un "entrenador" del modelo
+trainer = Trainer(
+    model=model,
+    args=trainingArgs,
+    train_dataset=tokenizedDataset["train"],
+    eval_dataset=tokenizedDataset["test"],
+    compute_metrics=obtainAccuracy,
+)
 
-# TODO 6: Entrena/refina el modelo, evalúa su rendimiento y muestra su precisión
+# 6: Entrena/refina el modelo, evalúa su rendimiento y muestra su precisión
+trainer.train()
+evalResults = trainer.evaluate()
+print(evalResults)
